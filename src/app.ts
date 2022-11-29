de76713c8b96b4c473a10c6cfdbcb2b4db7f3364
Francisco Santana Neto <fsn.santana@hotmail.com>
2022-11-28 23:24:44 -0400

    Vuvuzela2 created

--- src/app.ts
+++ src/app.ts
@@ -15,11 +15,13 @@ export default class App {
         private assets: AssetContainer;
         private mouths: Map<Guid, Actor>;
         private vuvuzelas: Map<Guid, Actor>;
+        private choosedVuvuzela: Map<Guid, String>;
 	private buzzing: Map<Guid, Boolean>;
         constructor(private context: Context, params: ParameterSet) {
                 this.assets = new AssetContainer(this.context);
                 this.mouths = new Map<Guid, Actor>();
                 this.vuvuzelas = new Map<Guid, Actor>();
+                this.choosedVuvuzela = new Map<Guid, String>();
                 this.buzzing = new Map<Guid, Boolean>();
                 this.context.onStarted(() => this.started());
                 this.context.onUserJoined((u: User) => this.userjoined(u));
@@ -33,8 +35,19 @@ export default class App {
 
         }
 
-        private createVuvuzela(user: User) {
+        private createChoosedVuvuzela(user: User, choosed: String){
+                if ( choosed == "vuvuzela1" ){
+                        this.createVuvuzela1(user)
+                };
+                if ( choosed == "vuvuzela2" ){
+                        this.createVuvuzela2(user)
+                };
+
+        }
+
+        private createVuvuzela1(user: User) {               
                 if (this.vuvuzelas.has(user.id)) return;
+                this.choosedVuvuzela.set(user.id,"vuvuzela1")
                 const vuvuzela = Actor.CreateFromLibrary(this.context, {
                         resourceId: 'artifact:2143270789623841562',
                         actor: {
@@ -55,7 +68,7 @@ export default class App {
 
                 const trigger = Actor.Create(this.context, {
                         actor: {
-				name: "vuvuzela_trigger",
+				name: "vuvuzela1",
 				owner: user.id,
                                 parentId: vuvuzela.id,
                                 appearance: {
@@ -92,18 +105,92 @@ export default class App {
                 });
         }
 
-        private buzz(user: User){
-		if ( this.buzzing.get(user.id) ) { return; }
-		this.buzzing.set(user.id,true);
-                const actor = Actor.CreateFromLibrary(this.context, {
-                        resourceId: 'artifact:2144568407004021116',
+        private createVuvuzela2(user: User) {               
+                if (this.vuvuzelas.has(user.id)) return;
+                this.choosedVuvuzela.set(user.id,"vuvuzela2")
+                const vuvuzela = Actor.CreateFromLibrary(this.context, {
+                        resourceId: 'artifact:2143270789623841562',
                         actor: {
+                                transform: {
+                                        local: {
+                                                position: {x: -0.1, y: 0, z: 0.08},
+                                                rotation: Quaternion.FromEulerAngles(-90*DegreesToRadians,0,-90*DegreesToRadians),
+						scale: { x: 0.35, y: 0.35, z: 0.35 }
+                                        }
+                                },
                                 attachment: {
                                         userId: user.id,
                                         attachPoint: 'left-hand'
                                 }
                         }
                 });
+		this.vuvuzelas.set(user.id,vuvuzela);
+
+                const trigger = Actor.Create(this.context, {
+                        actor: {
+				name: "vuvuzela2",
+				owner: user.id,
+                                parentId: vuvuzela.id,
+                                appearance: {
+                                        meshId: this.assets.createBoxMesh('box', 0.03, 0.03, 0.03).id,
+                                        materialId: this.assets.createMaterial('invisible', {
+                                                color: Color4.FromColor3(Color3.Red(), 0.3),
+                                                alphaMode: AlphaMode.Blend
+                                        }).id
+                                },
+                                transform: {
+                                        local: {
+                                                position: {x: 0, y: 0.6, z: 0}
+                                        }
+                                },
+                                collider: {
+                                        geometry: { shape: ColliderType.Box},
+                                        layer: CollisionLayer.Hologram,
+                                        isTrigger: true
+                                },
+                                rigidBody: {
+                                        enabled: true,
+                                        useGravity: false,
+                                        isKinematic: true
+                                }
+
+                        }
+                });
+
+                trigger.collider.onTrigger('trigger-enter', (actor: Actor) => {
+			console.log("\n",actor.name," - ", actor.owner);
+			console.log(trigger.name," - ",trigger.owner);
+                        if ( actor.name != 'mouth' || actor.owner != trigger.owner ) { return; }
+                        this.buzz(user);
+                });
+        }
+
+        private buzz(user: User){
+		if ( this.buzzing.get(user.id) ) { return; }
+		this.buzzing.set(user.id,true);
+
+                if ( this.choosedVuvuzela.get(user.id) == "vuvuzela1" ) {
+                        const actor = Actor.CreateFromLibrary(this.context, {
+                                resourceId: 'artifact:2144568407004021116',
+                                actor: {
+                                        attachment: {
+                                                userId: user.id,
+                                                attachPoint: 'left-hand'
+                                        }
+                                }
+                        });
+                };
+                if ( this.choosedVuvuzela.get(user.id) == "vuvuzela2" ) {
+                        const actor = Actor.CreateFromLibrary(this.context, {
+                                resourceId: 'artifact:2145512382976230004',
+                                actor: {
+                                        attachment: {
+                                                userId: user.id,
+                                                attachPoint: 'left-hand'
+                                        }
+                                }
+                        });
+                };
 
                 setTimeout(()=>{
 			this.buzzing.set(user.id,false);
@@ -151,12 +238,15 @@ export default class App {
                 this.showMenu();
         }
 
-        private toogleVuvuzela(user: User){
+        private toogleVuvuzela(user: User,choosed: String){
                 if (this.vuvuzelas.has(user.id)) {
                         this.userleft(user);
-                        return;
+                        if ( choosed == this.choosedVuvuzela.get(user.id)){
+                                this.choosedVuvuzela.delete(user.id)
+                                return; 
+                        }
                 };
-                this.createVuvuzela(user);
+                this.createChoosedVuvuzela(user,choosed);
                 this.createMouth(user);
         }
 
@@ -176,7 +266,7 @@ export default class App {
 	private showMenu() {
 		const menu = Actor.Create(this.context);
 
-		var button;
+		var button1, button2;
 
                 const vuvuzela1 = Actor.CreateFromLibrary(this.context, {
                         resourceId: 'artifact:2143270789623841562',
@@ -196,7 +286,7 @@ export default class App {
                         }
                 });
 
-                button = Actor.CreatePrimitive(this.assets, {
+                button1 = Actor.CreatePrimitive(this.assets, {
                         definition: {
                                 shape: PrimitiveShape.Box,
                                 dimensions: { x: 0.3, y: 1.2, z: 0.3 }
@@ -204,7 +294,7 @@ export default class App {
                         addCollider: true,
                         actor: {
                             parentId: menu.id,
-                            name: "vuvuzela1_button",
+                            name: "vuvuzela1",
                             transform: {
                                 local: {
                                     position: { x: 0, y: 0, z: 0 },
@@ -215,8 +305,48 @@ export default class App {
                                 enabled: false
                             }
                         }
+                });
+                const vuvuzela2 = Actor.CreateFromLibrary(this.context, {
+                        resourceId: 'artifact:2143270789623841562',
+                        actor: {
+                                name: "menu_vuvuzela2",
+                                parentId: menu.id,
+                                transform: {
+                                        local: {
+                                                position: { x: 0, y: 1, z: 0 },
+                                                rotation: Quaternion.FromEulerAngles(
+                                                0 * DegreesToRadians,
+                                                0 * DegreesToRadians,
+                                                0 * DegreesToRadians),
+                                                scale: { x: 0.75, y: 0.75, z: 0.75 }
+                                        }
+                                }
+                        }
+                });
+
+                button2 = Actor.CreatePrimitive(this.assets, {
+                        definition: {
+                                shape: PrimitiveShape.Box,
+                                dimensions: { x: 0.15, y: 0.6, z: 0.15 }
+                        },
+                        addCollider: true,
+                        actor: {
+                            parentId: menu.id,
+                            name: "vuvuzela2",
+                            transform: {
+                                local: {
+                                    position: { x: 0, y: 1, z: 0 },
+                                    scale: { x: 1, y: 1, z: 1 }
+                                }
+                            },
+                            appearance: {
+                                enabled: false
+                            }
+                        }
                     });
 
-                button.setBehavior(ButtonBehavior).onClick(user => this.toogleVuvuzela(user));
+
+                button1.setBehavior(ButtonBehavior).onClick(user => this.toogleVuvuzela(user,"vuvuzela1"));
+                button2.setBehavior(ButtonBehavior).onClick(user => this.toogleVuvuzela(user,"vuvuzela2"));
 	}
 }
